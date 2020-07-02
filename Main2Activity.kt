6473db9dbc833ac1a2268f69f3a9194c6d7e26e1
mri86101 <57848802+mri86101@users.noreply.github.com>
2020-07-03 08:16:33 +0900

    first commit

---
+++ Main2Activity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication1
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+    }
+}
---
+++ MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication1
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            val nextIntent = Intent(this, Main2Activity::class.java)
+            startActivity(nextIntent)
+        }
+    }
+
+}
